cdae649368588a246c960e8e73c5ca74da7cabc1
MykytaPlatonov <ipz222_pms@student.ztu.edu.ua>
2026-02-02 21:58:52 +0200

    Feature: Template Method - Lifecycle hooks

---
+++ study/КПЗ/mkr/BehavioralPatterns/BehavioralPatterns/Patterns/TemplateMethod/LifecycleDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BehavioralPatterns.Core;
+
+namespace BehavioralPatterns.Patterns.TemplateMethod
+{
+    public class LifecycleDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("\n=== 1. Шаблонний метод (Template Method) ===");
+            Console.WriteLine("Демонстрація lifecycle hooks:");
+
+            var div = new LightElementNode("div");
+            div.Classes.Add("container");
+            div.Classes.Add("main");
+
+            var text = new LightTextNode("Hello, World!");
+            div.AddChild(text);
+
+            Console.WriteLine("\nРендер елемента:");
+            var html = div.Render();
+            Console.WriteLine($"Результат: {html}");
+
+            div.OnRemoved();
+        }
+    }
+}
